3f179d9350d67afd410471f288b6301b47fc4964
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-04-10 20:34:11 +0100

    Some quick changes

---
+++ Tests/adjancencyList.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+
+typedef struct adjNode adjNode;
+struct adjNode {
+	int data;
+	adjNode *next;
+};
+
+struct adjList {
+	adjNode *head;
+};
+
+struct Graph {
+
+
+int main()
+{
+	return 0;
+}
