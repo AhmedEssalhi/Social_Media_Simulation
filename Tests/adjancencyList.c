06fc8b02086fa343ff4112b0f76161e9a0eb41a3
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-04-12 00:14:59 +0100

    My local commits

--- Tests/adjancencyList.c
+++ Tests/adjancencyList.c
@@ -1,21 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX 10
 
 typedef struct adjNode adjNode;
 struct adjNode {
-	int data;
+	int dest;
+	char *name;
 	adjNode *next;
 };
 
-struct adjList {
+typedef struct {
 	adjNode *head;
-};
+}adjList;
 
-struct Graph {
+typedef struct {
+	int V;
+	int pos;
+	adjList *arrayPtr;
+}graph;
 
+graph *createEmptyGraph()
+{
+	graph *arrayList;
+	int i;
+
+	arrayList->arrayPtr = (adjList *)malloc(sizeof(adjList) * MAX);
+	arrayList->pos = 0;
+	for (i = 0; i < MAX; i++)
+		arrayList->arrayPtr[i].head = NULL;
+	return arrayList;
+}
+
+graph *createUser()
+{
+	graph *g;
+	adjNode *user;
+	
+	user = malloc(sizeof(adjNode));
+	user->next = NULL;
+	g->arrayPtr[g->pos].head = user;
+	user = g->arrayPtr[g->pos++].head;
+	printf("Username: ");
+	scanf("%[^\n]s", user->name);
+
+	return g;
+}
 
 int main()
 {
+	adjList *linkedList;
+	graph *arrayList;
+	adjNode *node;
+	int i;
+
+	printf("Hello helberton\n");
+	arrayList = createEmptyGraph();
+	printf("In the middle of something\n");
+	arrayList = createUser();
+	/*for (i = 0; i < 2; i++)*/
+		printf("Username: %s\n", 
+				arrayList->arrayPtr[i].head->name);
+	printf("Happy Coding\n");
+
+
 	return 0;
 }
