53c50ba4bc195c0e9f784992c1425292df816b41
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-04-21 18:45:40 +0100

    Fusioner les deux branch 'main' et 'rida'

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main() {
         printf("\nMenu :\n");
         printf("1. Ajouter un utilisateur\n");
         printf("2. Quitter\n");
-	    printf("3. Afficher la liste\n");
+	printf("3. Afficher la liste\n");
         printf("4. creer ami(e)\n");
         printf("Entrez votre choix : ");
         scanf("%d", &choix);
